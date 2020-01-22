5f6ebcbd4b362c79d5eeb26fbc9c3e0c8e3dfca6
Viranchee <viranchee@outlook.com>
2020-01-22 16:12:40 +0530

    Initial Commit

---
+++ PrivatePropertiesInProtocols/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PrivatePropertiesInProtocols
+//
+//  Created by Viranchee L on 22/01/20.
+//  Copyright © 2020 Viranchee L. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
